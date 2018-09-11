670fe919c5d250827fbf8395ed3a9a294f95a9eb
wbusaka <wilbusaka@gmail.com>
2018-09-11 11:47:43 +0000

    Show newest tasks at the top

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -4,7 +4,8 @@ import './body.html';
  
 Template.body.helpers({
  tasks() {
-    return Tasks.find({});
+   // Show newest tasks at the top
+   return Tasks.find({}, { sort: { createdAt: -1 } });
   },
 });
 
