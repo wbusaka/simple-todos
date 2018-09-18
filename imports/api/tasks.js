cba14ae9de960cca8093a462f260e15b626d316e
wbusaka <wilbusaka@gmail.com>
2018-09-18 11:41:19 +0000

    Add publication for tasks

--- imports/api/tasks.js
+++ imports/api/tasks.js
@@ -3,7 +3,18 @@ import { Mongo } from 'meteor/mongo';
 import { check } from 'meteor/check';
 
 export const Tasks = new Mongo.Collection('tasks');
- 
+
+if (Meteor.isServer) {
+
+  // This code only runs on the server
+
+  Meteor.publish('tasks', function tasksPublication() {
+
+    return Tasks.find();
+
+  });
+
+}
 Meteor.methods({
   'tasks.insert'(text) {
     check(text, String);
